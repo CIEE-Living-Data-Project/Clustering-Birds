23c6d8a0105edc5dcbcab72db297496939383e81
fieldima <60451947+fieldima@users.noreply.github.com>
2022-09-18 15:35:38 -0700

    Add a line to the download script create the data directory

--- scripts/download.R
+++ scripts/download.R
@@ -10,9 +10,13 @@ groundhog::groundhog.library(
   ,'2022-09-01', tolerate.R.version = '4.1.2'
 )
 
+#Create the data directory
+if (!dir.exists("data")) dir.create("data")
+
 # Now to download from Dryad from the following doi: https://doi.org/10.5061/dryad.1rn8pk0tb
 
 rdryad::dryad_download("10.5061/dryad.1rn8pk0tb")
-system("cp -r /home/fieldima/.cache/R/rdryad/10_5061_dryad_1rn8pk0tb/ data/")
 
-# Change the file paths to wherever the system downloads them to
+# This calls bash on the terminal to move the data into the correct directory. In windows, might have to be done manually.
+# Change the file paths below to wherever the system downloads them to
+system("cp -r /home/fieldima/.cache/R/rdryad/10_5061_dryad_1rn8pk0tb/ data/")
